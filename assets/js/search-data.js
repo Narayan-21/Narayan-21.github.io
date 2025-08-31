56bc96510a8ef0afe86aa14b06053b2b8d99c428
Narayan-21 <64371700+Narayan-21@users.noreply.github.com>
2025-08-31 06:00:08 +0000

    Deploying to gh-pages from @ Narayan-21/Narayan-21.github.io@5390dbc4764f3c6a41098496fc810b6df1c82496 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -117,17 +117,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/images/";
           
         },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
@@ -135,14 +124,6 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
