29724f28c290d37019c600cb36a9ad806aaf6794
Narayan-21 <64371700+Narayan-21@users.noreply.github.com>
2025-09-12 21:19:51 +0000

    Deploying to gh-pages from @ Narayan-21/Narayan-21.github.io@6a0fa20b3ecd4009bb38cbf4a0937e55b7a0b823 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-publications",
-          title: "publications",
-          description: "Coming soon",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
         },{id: "nav-readings",
           title: "Readings",
           description: "",
